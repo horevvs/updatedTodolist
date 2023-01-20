1da9a1eb4b4a16a9b2e6f4cd1e315ace87f6ac09
Vladimir Horev <VShorev@yandex.ru>
2023-01-20 12:54:42 +0300

    added
    добавил функцию добавления выполнения

--- src/App.js
+++ src/App.js
@@ -18,30 +18,34 @@ function App() {
   }, [])
 
 
+  useEffect(() => {
+
+  }, [])
 
 
 
 
   //
-  const ref = useRef(null);
-  const checkbox = useRef(null);
+  // const ref = useRef(null);
+  // const checkbox = useRef(null);
   //
   const [inputs, setInputs] = useState([])
   const [list, setList] = useState([])
   const [edit, setEdit] = useState([])
-  const [returnedit, setReturnedit] = useState([])
+  // const [returnedit, setReturnedit] = useState([])
   const [returnara, setReturnara] = useState([])
+
+
   // добавление элемента с интпута //
 
 
 
   const addfrominput = () => {
-
     let random = Math.random().toFixed(2) * 100
     setList([...list, { value: inputs, id: random }])
     setTodo([...todo, { name: inputs, id: random }])
     // просто складываем сюда тоже самое что и в лист
-    setReturnedit([...returnedit, { value: inputs, id: random }])
+    // setReturnedit([...returnedit, { value: inputs, id: random }])
     // ищем последний элемент масссива, чтобы  положить  отправить его на сервер.
 
     let index = list.at(-1);
@@ -101,10 +105,41 @@ function App() {
     document.location.reload();
   }
 
-  // толи нужен толи нет, потом допилить//
-  let check = (id) => {
-    // alert(id)
-    // checkbox.current.style.textDecoration = 'line-through'
+  //  подтверждение выполнения//
+  const handle = (id) => {
+    alert(id)
+
+    let a = todo
+    for (let i = 0; i < todo.length; i++)
+      if (todo[i].id == id) {
+        a[i].name = 'выполнено'
+        setList(a)
+
+
+        let obj = {
+          name: a[i].name,
+        }
+        let thisname = todo[i].id
+
+        let url = `https://todo.soprano.biz/note/${thisname}`
+        fetch(url, {
+          method: 'PUT',
+          body: JSON.stringify(obj),
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          }
+        }
+        )
+      
+        
+  
+      
+      
+
+
+
+
+      }
   }
 
   let returndeleted = (id) => {
@@ -118,42 +153,32 @@ function App() {
     // проверяем что старый запрос появился
 
     // перебираем масссив ищем старое имя котрое было по id  в note_id
-    for (let i = returnara.length - 1; i < returnara.length; i--)
-      if (returnara[i].note_id == id) {
+    // for (let i = returnara.length - 1; i < returnara.length; i--)
+    for (let i = returnara.length - 1; i > 0; i--)
+      if (returnara[i].note_id === id) {
         let b = returnara[i].subject
         let a = returnara[i].note_id
-
         for (let c = 0; c < todo.length; c++)
           if (todo[c].id === a) {
-
-
             todo[c].name = b
             console.log(a + b)
           }
       }
-
-
-
-
     //document.location.reload();
-
-
   }
 
   return (
     <div >
-      <div className='position'>
-        <TextField id="standard-basic" label="заметки" variant="standard" ref={ref} value={inputs} onChange={(e) => setInputs(e.target.value)} />
+         <div className='position'>
+        <TextField id="standard-basic" label="заметки" variant="standard" value={inputs} onChange={(e) => setInputs(e.target.value)} />
         <Button onClick={addfrominput} variant="contained" endIcon={<SendIcon />}> Send  </Button>
       </div>
       {todo.map((item) => {
         return (
           <div key={item.id} >
-            <div className='flow positionreder'> <input type="checkbox" onClick={() => check(item.id)} ref={checkbox}  ></input>
-              <p> {item.name}   </p>
-              <div>
-                <button onClick={() => deletehandler(item.id)}> удалить</button>
-              </div>
+            <div className='flow positionreder'>  <p onClick={() => { handle(item.id); }}> {item.name} </p> <div>
+              <button onClick={() => deletehandler(item.id)}> удалить</button>
+            </div>
               <div className='opasity'>
                 <input type="text" placeholder='add text' value={edit} onChange={(e) => setEdit(e.target.value)} />
                 <Button onClick={() => send(item.id, item.name)} >  edit </Button>
